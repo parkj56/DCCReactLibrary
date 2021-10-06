9e1d1ecd57f4eefbfaf92f92534593bbdd05eb77
DESKTOP-EJDVHOT\parkj <park.james1030@gmail.com>
2021-10-05 21:54:49 -0500

    completed previous and next book functionality.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,17 +14,45 @@ class App extends Component {
             booknumber:0
         };
     }
+
+    goToNextBook=() => {
+        let tempBookNumber= this.state.booknumber;
+        tempBookNumber++;
+        if(tempBookNumber=== this.books.length){
+            tempBookNumber= 0;
+        }
+        this.setState({
+            booknumber: tempBookNumber
+        });
+    }
+
+    goToPreviousBook= () => {
+        let tempBookNumber= this.state.booknumber;
+        tempBookNumber--;
+        if(tempBookNumber<0){
+            tempBookNumber= this.books.length -1;
+        }
+        this.setState({
+            booknumber: tempBookNumber
+        })
+
+    }
+
     render(){
         return(
             <div className= 'container-fluid'>
                 <TitleBar />
                 <div className= 'row'>
-                    <div className='col-md-4'></div>
+                    <div className='col-md-4'>
+                    <button onClick={this.goToPreviousBook}>Previous Book</button>
+                    </div>
                     <div className='col-md-4'>
                         <h1>{this.books[this.state.booknumber].title}</h1>
                         <h4>{this.books[0].author}</h4>
                     </div>
-                        <div className='col-md-4'></div>
+                        <div className='col-md-4'>
+                            <button onClick={this.goToNextBook}>Next Book</button>
+                        </div>
                     </div>
  
             </div>
